6ab07285575bd4681acb7fe1db646457cfcacd99
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-06-06 14:11:00 +0530

    Add basic layout

--- Ticket-System/src/App.tsx
+++ Ticket-System/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0);
+  const [history, setHistory] = useState<string[]>([]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Ticket System</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div className="submit">
+          <h2>Home Page</h2>
+          <button>Add Ticket</button>
+        </div>
+        <div className="history-section">
+          <div className="container">
+            {history.length === 0 ? (
+              <p>No Tickets</p>
+            ) : (
+              <>
+                {history.map((old) => {
+                  return <p>{old}</p>;
+                })}
+              </>
+            )}
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
