636a002d121b49ae51adf1ccc98690e4569fcf29
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-06-06 14:24:58 +0530

    Add modal for adding tickets

--- Ticket-System/src/App.tsx
+++ Ticket-System/src/App.tsx
@@ -1,17 +1,55 @@
 import { useState } from "react";
 import "./App.css";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#root");
 
 function App() {
-  const [count, setCount] = useState<number>(0);
   const [history, setHistory] = useState<string[]>([]);
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
 
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "black";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <>
       <h1>Ticket System</h1>
       <div className="card">
         <div className="submit">
           <h2>Home Page</h2>
-          <button>Add Ticket</button>
+          <button onClick={openModal}>Add Ticket</button>
+          <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+            <button onClick={closeModal}>close</button>
+            <div>I am a modal</div>
+          </Modal>
         </div>
         <div className="history-section">
           <div className="container">
